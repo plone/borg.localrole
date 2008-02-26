3ab85331cf0e2b6fb87bd15a40053aec5f3a0d8a
Alec Mitchell <apm13@columbia.edu>
2008-02-26 09:23:28 +0000

    Don't require product installation for plugin install
    svn path=/borg/components/borg.localrole/trunk/; revision=59445

--- borg/localrole/utils.py
+++ borg/localrole/utils.py
@@ -4,6 +4,7 @@ from Products.CMFCore.utils import getToolByName
 from Products.PlonePAS.Extensions.Install import activatePluginInterfaces
 
 from borg.localrole.config import LOCALROLE_PLUGIN_NAME
+from borg.localrole.workspace import manage_addWorkspaceLocalRoleManager
 
 def setup_localrole_plugin(portal):
     """Install and prioritize the local-role PAS plug-in
@@ -12,11 +13,10 @@ def setup_localrole_plugin(portal):
     
     uf = getToolByName(portal, 'acl_users')
 
-    localrole = uf.manage_addProduct['borg.localrole']
     existing = uf.objectIds()
 
     if LOCALROLE_PLUGIN_NAME not in existing:
-        localrole.manage_addWorkspaceLocalRoleManager(LOCALROLE_PLUGIN_NAME)
+        manage_addWorkspaceLocalRoleManager(uf, LOCALROLE_PLUGIN_NAME)
         activatePluginInterfaces(portal, LOCALROLE_PLUGIN_NAME, out)
     else:
         print >> out, "%s already installed" % LOCALROLE_PLUGIN_NAME
