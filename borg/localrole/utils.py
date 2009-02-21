a1545bd8a58a1bad70543262cf78e7695fef59da
Hanno Schlichting <hanno@hannosch.eu>
2009-02-21 23:54:50 +0000

    Added the `replace_local_role_manager` method formerly found in CMFPlone.
    svn path=/borg/components/borg.localrole/trunk/; revision=81160

--- borg/localrole/utils.py
+++ borg/localrole/utils.py
@@ -1,7 +1,9 @@
 from StringIO import StringIO
 
+from Acquisition import aq_base
 from Products.CMFCore.utils import getToolByName
 from Products.PlonePAS.Extensions.Install import activatePluginInterfaces
+from Products.PlonePAS.plugins.local_role import LocalRolesManager
 
 from borg.localrole.config import LOCALROLE_PLUGIN_NAME
 from borg.localrole.workspace import manage_addWorkspaceLocalRoleManager
@@ -22,3 +24,17 @@ def setup_localrole_plugin(portal):
         print >> out, "%s already installed" % LOCALROLE_PLUGIN_NAME
         
     return out.getvalue()
+
+def replace_local_role_manager(portal):
+    """Installs the borg local role manager in place of the standard one from
+    PlonePAS"""
+    uf = getToolByName(portal, 'acl_users', None)
+    # Make sure we have a PAS user folder
+    if uf is not None and hasattr(aq_base(uf), 'plugins'):
+        # Remove the original plugin if it's there
+        if 'local_roles' in uf.objectIds():
+            orig_lr = getattr(uf, 'local_roles')
+            if isinstance(orig_lr, LocalRolesManager):
+                uf.plugins.removePluginById('local_roles')
+        # Install the borg.localrole plugin if it's not already there
+        setup_localrole_plugin(portal)
