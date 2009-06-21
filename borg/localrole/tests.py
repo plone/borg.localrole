2f9725d31204c9a49ecb8dac83c20ef86bbf348b
Hanno Schlichting <hanno@hannosch.eu>
2009-06-21 13:01:23 +0000

    Adjusted test setup for Plone 5.
    svn path=/borg/components/borg.localrole/trunk/; revision=88563

--- borg/localrole/tests.py
+++ borg/localrole/tests.py
@@ -51,30 +51,19 @@ class DummyUser(object):
         return ()
 
 @onsetup
-def setup_product():
-    """Set up the additional products required for this package.
-    
-    The @onsetup decorator causes the execution of this body to be deferred
-    until the setup of the Plone site testing layer.
-    """
-    
-    # Load the ZCML configuration for dependent packages.
-    
+def setup_package():
     fiveconfigure.debug_mode = True
     zcml.load_config('configure.zcml', borg.localrole)
     fiveconfigure.debug_mode = False
-    
-    # We need to tell the testing framework that these products
-    # should be available. This can't happen until after we have loaded
-    # the ZCML.
-    ztc.installPackage('borg.localrole')
-    
-# The order here is important: We first call the (deferred) function which
-# installs the products we need.Then, we let PloneTestCase set up this 
-# product on installation.
 
-setup_product()
-ptc.setupPloneSite(extension_profiles=['borg.localrole:default'])
+    ztc.installPackage('borg.localrole')
+
+
+setup_package()
+ptc.setupPloneSite(extension_profiles=(
+    'Products.ATContentTypes:default',
+    'borg.localrole:default',
+))
 
 def test_suite():
     suite = []
