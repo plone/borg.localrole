4f2991d5db8c53964067bb2d81b7b202fd9890fb
Maurits van Rees <maurits@vanrees.org>
2016-02-09 23:14:47 +0100

    reindent setup.py

--- setup.py
+++ setup.py
@@ -6,28 +6,29 @@ version = '3.1.2.dev0'
 readme = open('README.txt').read()
 history = open('CHANGES.txt').read()
 
-setup(name = name,
-      version = version,
-      description = 'A PAS plugin which can manage local roles via an '
-                    'adapter lookup on the current context',
-      long_description = readme + '\n' + history,
-      keywords = 'Plone PAS local roles',
-      author = 'Borg Collective',
-      author_email = 'borg@plone.org',
-      url = 'https://pypi.python.org/pypi/borg.localrole',
-      license = 'LGPL',
-      packages = find_packages(exclude=['ez_setup']),
-      namespace_packages = ['borg'],
-      include_package_data = True,
-      platforms = 'Any',
-      zip_safe = False,
-      extras_require=dict(
-        test=[
-            'Products.ATContentTypes',
-            'plone.app.testing',
-        ]
-      ),
-      install_requires=[
+setup(
+    name=name,
+    version=version,
+    description='A PAS plugin which can manage local roles via an '
+                'adapter lookup on the current context',
+    long_description=readme + '\n' + history,
+    keywords='Plone PAS local roles',
+    author='Borg Collective',
+    author_email='borg@plone.org',
+    url='https://pypi.python.org/pypi/borg.localrole',
+    license='LGPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['borg'],
+    include_package_data=True,
+    platforms='Any',
+    zip_safe=False,
+    extras_require=dict(
+      test=[
+          'Products.ATContentTypes',
+          'plone.app.testing',
+      ]
+    ),
+    install_requires=[
         'setuptools',
         'zope.annotation',
         'zope.component',
@@ -40,18 +41,18 @@ setup(name = name,
         'plone.memoize',
         'Acquisition',
         'Zope2',
-      ],
-      classifiers = [
+    ],
+    classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Plone',
         'Framework :: Plone :: 5.0',
         'Intended Audience :: Other Audience',
         'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',  # noqa
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
-      ],
+    ],
 )
